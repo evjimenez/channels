016f8512f3af1f73bb48af6f298d735705bc2dcd
ever <ever.jimenez@lifemiles.com>
2023-10-15 21:48:11 -0600

    First Commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, l := range links {
+		go checkLink(l, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("%v might be down!\n", link)
+		c <- link
+		return
+	}
+
+	fmt.Printf("%v is up\n", link)
+	c <- link
+}
